043ef47e179cc3a3a35435b6e2dba5cb3268b5cd
Floris Dobber <floris.dobber@finn.auto>
2022-11-29 13:44:50 +0100

    More prints

--- pre_commit_dbt/check_model_has_description.py
+++ pre_commit_dbt/check_model_has_description.py
@@ -21,13 +21,14 @@ def has_description(paths: Sequence[str], manifest: Dict[str, Any]) -> int:
 
     status_code = 0
     ymls = get_filenames(paths, [".yml", ".yaml"])
+    print("ymls", ymls)
     sqls = get_model_sqls(paths, manifest)
+    print("sqls", sqls)
     filenames = set(sqls.keys())
+    print(filenames)
 
     # get manifest nodes that pre-commit found as changed
     models = get_models(manifest, filenames)
-    for model in models:
-        print(model)
 
     # if user added schema but did not rerun the model
     schemas = get_model_schemas(list(ymls.values()), filenames)
